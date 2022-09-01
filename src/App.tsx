7d875e79cf646102deaf285d127699c912bc3e3e
Alvin Ng <alvin@dsaid.gov.sg>
2022-09-01 16:58:32 +0800

    chore(semicolon): remove semicolons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
-import Map, { Source, Layer, LineLayer, } from "react-map-gl";
+import { useEffect, useState } from "react"
+import Map, { Source, Layer, LineLayer, FullscreenControl } from "react-map-gl"
 
-import "./App.css";
-import maplibreGl from "maplibre-gl";
+import "./App.css"
+import maplibreGl from "maplibre-gl"
 
 const MAP_STYLE_URL = import.meta.env.VITE_MAP_STYLE_URL
 const MAP_REGIONS_URL = import.meta.env.VITE_MAP_REGIONS_URL
@@ -13,11 +13,11 @@ const lineMapLayer = {
     "line-color": 'white',
     "line-width": 1,
   }
-} as LineLayer;
+} as LineLayer
 
 function App() {
-  const [mapStyle, setMapStyle] = useState();
-  const [mapRegions, setMapRegions] = useState();
+  const [mapStyle, setMapStyle] = useState()
+  const [mapRegions, setMapRegions] = useState()
 
   useEffect(() => {
     fetch(MAP_STYLE_URL)
@@ -36,9 +36,9 @@ function App() {
   useEffect(() => {
     const token = localStorage.getItem('token')?.toString()
 
-    const authHeaders = new Headers();
+    const authHeaders = new Headers()
     authHeaders.append('Accept', 'application/vnd.geo+json')
-    if (token) authHeaders.append('Authorization', `Bearer ${token}`);
+    if (token) authHeaders.append('Authorization', `Bearer ${token}`)
 
     fetch(MAP_REGIONS_URL, { headers: authHeaders })
       .then((response) => response.json())
@@ -66,9 +66,10 @@ function App() {
         {mapRegions && <Source id="regions" type="geojson" data={mapRegions}>
           <Layer {...lineMapLayer}></Layer>
         </Source>}
+        <FullscreenControl />
       </Map>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
