13a0602b95c94355e9ebb768764d5451a1a7a7cb
Alvin Ng <alvin@dsaid.gov.sg>
2022-09-01 16:50:20 +0800

    chore(eslint): add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Map, { Source, Layer, LineLayer, } from "react-map-gl";
 
 import "./App.css";
