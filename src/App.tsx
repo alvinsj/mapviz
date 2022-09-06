0f70a45f8f065556da6bd20eda07eea74fabc259
Alvin Ng <alvin@dsaid.gov.sg>
2022-09-06 16:14:33 +0800

    feat(2maps): show 2 maps

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,18 @@ import Map from './components/Map'
 import './App.css'
 
 function App() {
-  return <Map id="map1" reuseMaps />
+  return (
+    <div
+      style={{
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'space-around',
+      }}
+    >
+      <Map id="map2" reuseMaps style={{ height: '90vh', width: '45vw' }} />
+      <Map id="map1" reuseMaps style={{ height: '90vh', width: '45vw' }} />
+    </div>
+  )
 }
 
 export default App
