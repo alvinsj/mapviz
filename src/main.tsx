a033198bba01c620cd399913ccda8c46e969a339
Alvin Ng <243186+alvinsj@users.noreply.github.com>
2023-07-01 16:49:45 +0800

    add sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { MapProvider } from "react-map-gl";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { MapProvider } from 'react-map-gl'
+import App from './App'
+import './index.scss'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <MapProvider>
       <App />
     </MapProvider>
   </React.StrictMode>
-);
+)
